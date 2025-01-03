13c76abfb0a924b52d3f5363a9f2c12edb2b66b7
Vikas Pandey <87011940+vikaspandey-001@users.noreply.github.com>
2025-01-04 00:18:08 +0530

    Delete music store data directory

--- music store data/Ques 2.sql
+++
@@ -1,6 +0,0 @@
--- Ques 2: Which countries have the most invoices?
-SELECT billing_country,
-COUNT(*) as total_invoice
-FROM invoice
-GROUP BY billing_country
-ORDER BY total_invoice DESC;
--- music store data/Ques 3.sql
+++
@@ -1,4 +0,0 @@
--- QUES 3: What are the top 3 values of total invoice?
-SELECT total FROM invoice
-ORDER BY total DESC
-LIMIT 3;
